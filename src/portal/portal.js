52bb4d8e1ec4872a0e17dbca018d159b080f651c
azatmagdeev <azatmagdeev@gmail.com>
2020-09-13 00:09:29 +0300

    forms for send data

--- src/portal/portal.js
+++ src/portal/portal.js
@@ -1,9 +1,44 @@
-async function getData(){
-    const data = await fetch('http://www.cbr.ru/scripts/XML_bic.asp',);
-    console.log(data);
-}
+// async function getData(){
+//     const data = await fetch('files/bic.xml',);
+//     console.log(data);
+// }
+//
+// getData().then(data=>{
+//     console.log(data);
+// });
 
-getData().then(data=>{
-    console.log(data);
+const xhr = new XMLHttpRequest();
+xhr.open('get','files/bic.json');
+xhr.addEventListener('load',()=>{
+    console.log(JSON.parse(xhr.responseText));
 });
+xhr.send();
+
+
+const formsContainer = document.getElementById('forms-container')
+const more = document.getElementById('more');
+more.addEventListener('click',()=>{
+    const sendForms = document.querySelectorAll('.sendDataForm');
+    const newForm = sendForms[sendForms.length-1].cloneNode(true);
+    for (let i = 0; i < newForm.length; i++) {
+        newForm[i].value = '';
+    }
+    formsContainer.appendChild(newForm);
+    sendForms[sendForms.length-1].style.display='block';
+});
+
+formsContainer.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const el = e.target;
+    if(el.className === 'btn btn-outline-warning  text-warning rounded-circle'){
+        console.log('eeeeeeeeeee')
+        el.closest('.sendDataForm').remove();
+    }else{
+        console.log('nononononono')
+    }
+})
+
+
+
+
 
