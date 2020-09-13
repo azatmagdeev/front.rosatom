3d80afafc01023c340ae6f5ca0630455c66ff99c
azat <azatmagdeev@gmail.com>
2020-09-13 13:24:17 +0300

    ping

--- src/portal/portal.js
+++ src/portal/portal.js
@@ -1,54 +1,102 @@
-// async function getData(){
-//     const data = await fetch('files/bic.xml',);
-//     console.log(data);
-// }
-//
-// getData().then(data=>{
-//     console.log(data);
-// });
 
 const xhr = new XMLHttpRequest();
-xhr.open('get','files/bic.json');
+xhr.open('get','http://3.126.220.47:8000/api/ping/');
 xhr.addEventListener('load',()=>{
-    console.log(JSON.parse(xhr.responseText));
+    console.log(xhr.responseText);
 });
 xhr.send();
 
 
-const formsContainer = document.getElementById('forms-container')
+const formsContainer = document.getElementById('forms-container');
 const more = document.getElementById('more');
-more.addEventListener('click',()=>{
-    const sendForms = document.querySelectorAll('.sendDataForm');
-    const newForm = sendForms[sendForms.length-1].cloneNode(true);
-    for (let i = 0; i < newForm.length; i++) {
-        newForm[i].value = '';
-    }
-    formsContainer.appendChild(newForm);
-    sendForms[sendForms.length-1].style.display='block';
-});
+// more.addEventListener('click',()=>{
+    // const sendForms = document.querySelectorAll('.sendDataForm');
+    // const newForm = sendForms[sendForms.length-1].cloneNode(true);
+    // for (let i = 0; i < newForm.length; i++) {
+    //     newForm[i].value = '';
+    // }
+    // formsContainer.appendChild(newForm);
+    // sendForms[sendForms.length-1].style.display='block';
+// });
 
 formsContainer.addEventListener('click',(e)=>{
     e.preventDefault();
     const el = e.target;
     if(el.className === 'btn btn-outline-warning  text-warning rounded-circle'){
-        console.log('eeeeeeeeeee')
+        console.log('eeeeeeeeeee');
         el.closest('.sendDataForm').remove();
     }else{
         console.log('nononononono')
     }
-})
+});
+
+const sendBtn = document.getElementById('send');
 
 
 class Report{
-   constructor(bic, date, amount, currency){
+   constructor(
+       bic,
+       date,
+       amount,
+       currency,
+       comment,
+   ){
+
         this.bic = bic;
         this.date = date;
         this.amount = amount;
         this.currency = currency;
+        this.comment = comment;
+        this.inn= '';
+        this.bankName = '';
+        this.startContract="";
+        this.activateContract="";
+        this.endContract=""; this.interest="";
+        this.moneyCod=currency;
+        this.moneyCount=amount;
+        this.uuid="";
+   }
+
+   send(){
+       console.log(this);
+       const xhr = new XMLHttpRequest();
+       xhr.open('post','http://3.126.220.47:8000/api/setContract',);
+
+       xhr.setRequestHeader('Content-Type', 'application/json');
+       xhr.addEventListener('load',()=>{
+           console.log(JSON.stringify(this));
+       });
+       xhr.send(JSON.stringify(this));
    }
 }
 
+sendBtn.addEventListener('click',()=>{
+    const form = formsContainer.querySelector('form');
+    const report = new Report(
+        form[1].value,
+        (new Date).toLocaleDateString(),
+        form[3].value,
+        form[2].value,
+        form[4].value,
+    );
+    report.send()
+});
 
+document.getElementById('my-reports').addEventListener('click',(e)=>{
+    // e.target.parent?e.target.parent.classList.remove('bg-light'):null;
+    // e.target.parent.classList.add('bg-info');
+
+    document.getElementById('send-report-main').style.display='none';
+    document.getElementById('my-reports-main').style.display='block';
+});
+
+document.getElementById('send-report').addEventListener('click',(e)=>{
+    //
+    // e.target.parent.classList.remove('bg-light');
+    // e.target.parent.classList.add('bg-info');
+    document.getElementById('send-report-main').style.display='block';
+    document.getElementById('my-reports-main').style.display='none';
+});
 
 
 
